1b93a0d4fac3a7e095bdd6d4c2ff4cf0bc675ff5
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-14 19:13:35 +0200

    user/dialog field created

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialogContent, MatDialogActions } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatButtonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogContent, MatDialogActions, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatDatepickerModule, FormsModule, ReactiveFormsModule],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogAddUserComponent {
-
+  readonly date = new FormControl();
+  readonly serializedDate = new FormControl(new Date().toISOString());
 }
