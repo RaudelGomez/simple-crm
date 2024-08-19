1091b24bb455c0cf7522945e140f466cd46e6ed3
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-19 13:50:54 +0200

    app.config/enviroment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,8 +7,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../enviroment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-f78a3","appId":"1:1025832021229:web:1569519abe3ea1146770e4","storageBucket":"simple-crm-f78a3.appspot.com","apiKey":"AIzaSyAFFmJurut8Kh57bOxt0rNXcFFjQd66xc0","authDomain":"simple-crm-f78a3.firebaseapp.com","messagingSenderId":"1025832021229"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
 
---
+++ src/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'simple-crm-f78a3',
+    appId: '1:1025832021229:web:1569519abe3ea1146770e4',
+    storageBucket: 'simple-crm-f78a3.appspot.com',
+    apiKey: 'AIzaSyAFFmJurut8Kh57bOxt0rNXcFFjQd66xc0',
+    authDomain: 'simple-crm-f78a3.firebaseapp.com',
+    messagingSenderId: '1025832021229'
+  }
+};
