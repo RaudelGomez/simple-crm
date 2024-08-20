40acba1803cbdb00f37baa97f0c0d471503b83b1
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-20 07:39:59 +0200

    user-detail/memory leaks problem result

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user.class';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
@@ -17,24 +18,36 @@ export class UserDetailComponent {
   userId!:string;
   user$?:User;
   loading: boolean = true;
+  private subscription: Subscription = new Subscription();
 
   constructor(private route: ActivatedRoute, private userService: UserService) { 
     this.userService.loadUsers();   
-   
   }
   
   ngOnInit() {
-     this.route.params.subscribe(params => {
-      this.userId = params['id'];
-      this.getUserDetails();
-    });
+    this.subscription.add(
+      this.route.params.subscribe(params => {
+        this.userId = params['id'];
+        this.getUserDetails();
+      })
+    );
   }
-   
-  getUserDetails(){
-    this.userService.users$.subscribe(users => {
+
+  ngOnDestroy(): void {
+    // Cancel all subscriptions when destroying the component.
+    this.subscription.unsubscribe();
+  }
+
+  getUserDetails() {
+    const userSubscription = this.userService.users$.subscribe(users => {
       this.user$ = users.find(user => user.id === this.userId);
+      console.log(this.user$);
       this.loading = false;
     });
-   }
 
+    // Add this subscription to the general handler.
+    this.subscription.add(userSubscription);
+  }
 }
+
+
