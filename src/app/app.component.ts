5e56297753672f33d2c91537922e00cbbba22ea9
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-27 21:45:14 +0200

    responsive/v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -14,4 +14,23 @@ import {MatIconModule} from '@angular/material/icon';
 })
 export class AppComponent {
   title = 'simple-crm';
+
+  isSmallScreen!: boolean;
+
+  /**
+   * Accesing to the brwoser to know when teh width changed
+   * @param platformId - Object from browser
+   */
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 1200;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth < 1200;
+    }
+  }
 }
