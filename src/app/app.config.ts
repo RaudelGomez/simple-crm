19115cefd6f99b9916751b5860eccb44f07342de
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-10-29 10:51:32 +0100

    test actions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -15,3 +16,26 @@ export const appConfig: ApplicationConfig = {
 };
 
 
+=======
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
+
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { environment } from "../environment/environment";
+
+export const appConfig: ApplicationConfig = {
+	providers: [
+		provideRouter(routes),
+		provideClientHydration(),
+		provideAnimationsAsync(),
+		importProvidersFrom(
+			provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+		),
+		importProvidersFrom(provideFirestore(() => getFirestore())),
+	],
+};
+>>>>>>> b911695 (test actions)
